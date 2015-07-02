9295311029b4158549c9a297babcde46dda515bb
Jacob Brenneman <j.brenneman19@gmail.com>
2015-07-02 17:49:26 -0600

    edited results page and added client side js for about button displaying bio

--- public/javascripts/dom.js
+++ public/javascripts/dom.js
@@ -6,6 +6,5 @@ for (var i = 0; i < about.length; i++) {
   connect[about[i].id] = bio[i];
   about[i].addEventListener('click', function () {
     connect[this.id].style.display = 'inline-block';
-    
   });
 }
