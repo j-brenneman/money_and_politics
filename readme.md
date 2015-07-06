62f6b0675910cdd6855aa37d1952b38d308147aa
Jacob Brenneman <j.brenneman19@gmail.com>
2015-07-06 13:21:18 -0600

    added a readme

---
+++ readme.md
@@ -0,0 +1,10 @@
+#Politcal Tracker
+
+This app allows a user to input a zip-code and election cycle and then displays candidates, vote count, and net campaign contributions for the correlating Congressional district and election year.
+
+https://ancient-brushlands-4281.herokuapp.com/
+
+Core Technologies
+  - Express
+  - Sunglight Labs API
+  - Mongo DB
--- routes/data.js
+++ routes/data.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var unirest = require('unirest');
 var sunlightLabs = require('./../lib/javascripts/sunlightLabs');
-var db = require('monk')(process.env.MONGOLAB_URI); //MONGO_URI=mongodb://heroku_7cbsx8f1:qokgrpo5t7uc6nnsg5qotk6vln@ds045521.mongolab.com:45521/heroku_7cbsx8f1
+var db = require('monk')('localhost/election_results'); //MONGO_URI=mongodb://heroku_7cbsx8f1:qokgrpo5t7uc6nnsg5qotk6vln@ds045521.mongolab.com:45521/heroku_7cbsx8f1
 var results2012 = db.get('results2012');
 var results2010 = db.get('results2010');
 var results2008 = db.get('results2008');
@@ -22,52 +22,57 @@ router.get('/results', function (req, res, next) {
 });
 // Retrieve User Input and Search DBS
 router.post('/input', function (req, res, next) {
-  var zipIN = req.body.zip;
-  var yearIN = 'results' + req.body.years;
-  var year = req.body.years;
-  districts.find({zip: parseInt(zipIN)}, function (err, data) {
-    var state = data[0].state;
-    if(String(data[0].district).length === 1)
-      var district = '0' + data[0].district;
-    else
-      var district = data[0].district;
-    var yrDB = db.get(yearIN);
-    yrDB.find({state: state, district: district}, function (err, datas) {
-      if(datas[0]) {
-        var result = [];
-        for(var i = 0; i < datas.length; i++) {
-          var percent = datas[i].votePercent;
-          sunlightLabs(datas[i].name, year, i, percent, function (reciepts, name, vote, photo, bio) {
-            var info = {};
-            if(reciepts === 'Information not Available')
-              var contributions = reciepts;
-            else
-              var contributions = '$' +' '+ reciepts.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,');
-            if(photo === 'not available' || photo === undefined || photo === '' )
-              var pic = 'Picture Unavailable';
-            else
-              var pic = photo;
+  var zipIN = req.body.zip.trim();
+  if(zipIN.length < 5 || zipIN.length > 5) {
+    res.render('input', {error: 'Please enter a Valid Zipcode'})
+  } else {
+    var yearIN = 'results' + req.body.years;
+    var year = req.body.years;
+    districts.find({zip: parseInt(zipIN)}, function (err, data) {
+      var state = data[0].state;
+      if(String(data[0].district).length === 1)
+        var district = '0' + data[0].district;
+      else
+        var district = data[0].district;
+      var yrDB = db.get(yearIN);
+      yrDB.find({state: state, district: district}, function (err, datas) {
+        if(datas[0]) {
+          var result = [];
+          for(var i = 0; i < datas.length; i++) {
+            var percent = datas[i].votePercent;
+            sunlightLabs(datas[i].name, year, i, percent, function (reciepts, name, vote, photo, bio) {
+              var info = {};
+              if(reciepts === 'Information not Available')
+                var contributions = reciepts;
+              else
+                var contributions = '$' +' '+ reciepts.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,');
+              if(photo === 'not available' || photo === undefined || photo === '' )
+                var pic = 'Picture Unavailable';
+              else
+                var pic = photo;
 
-            info.candidate = name;
-            info.reciepts = contributions;
-            info.percent = vote;
-            info.photo = pic;
-            info.bio = bio;
-            result.push(info);
-            console.log(result);
-            if(result.length === datas.length)
-              res.render('results', {info: result, state: state, district: district, year: year});
+              info.candidate = name;
+              info.reciepts = contributions;
+              info.percent = vote;
+              info.photo = pic;
+              info.bio = bio;
+              result.push(info);
+              console.log(result);
+              if(result.length === datas.length)
+                res.render('results', {info: result, state: state, district: district, year: year});
 
-          });
+            });
+          }
         }
-      }
-      else {
-        var message = 'Sorry but' +' '+ zipIN +' '+ 'currently represents' +
-                      ' '+ state +' '+ district +','+' '+ 'which did not exist in' +' '+ year;
-        res.render('input', {response: message});
-      }
+        else {
+          var message = 'Sorry but' +' '+ zipIN +' '+ 'currently represents' +
+                        ' '+ state +' '+ district +','+' '+ 'which did not exist in' +' '+ year;
+          res.render('input', {response: message});
+        }
+      });
+
     });
-  });
+  }
 });
 
 
